df5e0282f246f109909d6b4f698694c8d25d0fdd
NicolasAmorosi <nicolasamorosi@gmail.com>
2022-08-24 11:19:14 -0300

    Funcionaaaaaaaaaaaa

--- main.js
+++ main.js
@@ -1,26 +1,47 @@
 const qrcode = require('qrcode-terminal');
+const CronJob = require('cron').CronJob;
 
 const { Client, LocalAuth, ChatTypes } = require('whatsapp-web.js');
 const client = new Client({
     authStrategy: new LocalAuth()
 });
 
+//Generar Qr
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
 });
 
+//Aviso de inicio
 client.on('ready', () => {
     console.log('Client is ready!');
-    client.getChats().then(chats => {
-        const contact = chats.find((chat) => chat.name === "Papá");
-        client.sendMessage(contact.id._serialized, "Bata");
-    });
+    //Inicializa job del cron
+    initializeCron();
 });
 
+//Respuesta programable a un mensaje especifico
 client.on('message', message => {
 	if(message.body === '!info') {
 		message.reply('- Tenes 18 turnos de masajes esta semana bb');
 	}
 });
 
+//Configuracion cron 
+const job = new CronJob('* * * * * *', function() {
+    sendHB();
+});
+
+//Inicializa WhatsApp??
 client.initialize();
+
+//Mandar mensaje
+function sendHB(){
+    client.getChats().then(chats => {
+        const contact = chats.find((chat) => chat.name === "Rafael");
+        client.sendMessage(contact.id._serialized, "lindo");
+    });
+}
+
+//Funcion cron
+function initializeCron() {
+    job.start();
+}
